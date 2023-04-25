983d8062521fb06840da1ac5353a4ca505f72ed6
julilan <juli.molnar@outlook.com>
2023-04-25 13:55:54 +0300

    add box-shadow to active circle

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,15 +2,6 @@ import React from "react";
 import "./Circle.css";
 
 const Circle = (props) => {
-  //console.log(props);
-  // let style = {
-  //   backgroundColor: props.backgroundColor,
-  // };
-  // if (props.class === "circle active") {
-  //   style = {
-  //     backgroundColor: "#bf1363",
-  //   };
-  // }
   return <div className={props.class} onClick={props.click}></div>;
 };
 
